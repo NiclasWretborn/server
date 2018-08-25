372a9209ad017d88414c3738d9e26edc233099dc
Niclas Wretborn <NWretborn@gmail.com>
2018-08-25 02:41:23 +0200

    a

--- items/blog/index.php
+++ items/blog/index.php
@@ -61,7 +61,7 @@ $numImages = count($imgNames);
     $(document).ready(function(){
       var allImages = "";
       var numImages = <?php echo $numImages; ?>;
-        <?php echo $imgNames[2]; ?>;
+       var imgnamez = <?php echo $imgNames[2]; ?>;
   for (i=0; i<numImages; i++)
   {
     var width = getRandomSize(200, 400);
